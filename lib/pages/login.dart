ca3b5578b66abb479d092bb959cf613fe49a33c0
notekeven <jf5143437@gmail.com>
2024-06-23 19:59:55 -0300

    Adjusts Login page and navigation

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutterapp/main.dart';
 
 class Login extends StatelessWidget {
@@ -13,31 +12,83 @@ class Login extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Center(
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(20),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(8.0, 30.0, 8.0, 30.0),
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                ),
+                width: double.infinity,
+                height: 600,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Login E-commerce',
+                      style: TextStyle(
+                        fontSize: 25,
+                      ),
+                    ),
+                    const Text('Please sign in to continue'),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          suffixIcon: const Icon(Icons.email_outlined),
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(50)),
+                          // labelText: 'Enter your email',
+                          hintText: 'Enter your email',
+                          filled: true,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          suffixIcon: const Icon(Icons.lock_outline_rounded),
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(50)),
+                          // labelText: 'Enter your email',
+                          hintText: 'Enter your password',
+                          filled: true,
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: FilledButton(
+                            onPressed: () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        const MyHomePage(title: 'Holla'))),
+                            child: const Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text('LOGIN'),
+                                Icon(Icons.arrow_forward)
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
-              width: 300,
-              height: 600,
-              child: Column(
-                children: [
-                  const Text('Login'),
-                  TextButton(
-                      onPressed: () => Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const MyHomePage(
-                                    title: 'Holla',
-                                  ))),
-                      child: const Text('Ok'))
-                  // TextField(),
-                ],
-              ),
             ),
-          )
+          ),
         ],
       ),
     );
